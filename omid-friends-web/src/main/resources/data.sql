4d487e0d8f72c9176c58148a1ca69404aa6f336c
Omid Ashouri <omidashouri@gmail.com>
2019-09-16 11:29:47 -0700

    specify schema in data.sql file : omidashouri : 98.06.25

--- omid-friends-web/src/main/resources/data.sql
+++ omid-friends-web/src/main/resources/data.sql
@@ -2,17 +2,18 @@
 --drop schema if exists friends;
 --create schema friends;
 --show tables ;
+set schema friends;
 
-DROP TABLE IF EXISTS friends.billionaires;
+DROP TABLE IF EXISTS billionaires;
 
-CREATE TABLE friends.billionaires (
+CREATE TABLE billionaires (
       id INT AUTO_INCREMENT  PRIMARY KEY,
       first_name VARCHAR(250) NOT NULL,
       last_name VARCHAR(250) NOT NULL,
       career VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO friends.billionaires (first_name, last_name, career) VALUES
+INSERT INTO billionaires (first_name, last_name, career) VALUES
 ('Aliko', 'Dangote', 'Billionaire Industrialist'),
 ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
 ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
